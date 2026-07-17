f9773bbba2a0014df75c368d91d059ce83ce5130
Jean Estipz <jeantipez@gmail.com>
2026-07-17 00:38:55 +0000

    Adiciona scripts `lint:check` e ´lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>Obrigado Mãe e Pai por acreditar em mim, e me apoiar sempre! Eu amo muito vocês ❤️❤️</h1>
+function Home() {
+  return (
+    <h1>
+      Obrigado Mãe e Pai por acreditar em mim, e me apoiar sempre! Eu amo muito
+      vocês ❤️❤️
+    </h1>
+  );
 }
 
 export default Home;
